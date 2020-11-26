bc810f0bd26cf837fb63be9b86a1cffd79e9c104
Dc-cpu-arch <d.garzong77@gmail.com>
2020-11-26 02:34:28 +0000

    void

---
+++ void/shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - executes a loop that prints a prompt while true.
+ * @argc: number of arguments.
+ * @argv: vector of arguments.
+ * @envp: environ variable.
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char **argv, char *envp[])
+{
+	(void)argc, (void)argv;
+	printf("asdfasdf");
+	floop(envp);
+	return (EXIT_SUCCESS);
+}
